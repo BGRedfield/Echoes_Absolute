a0b02552114c2f659ffe6d553192be2afc4a126a
BG <arthurazulbg@gmail.com>
2026-09-17 15:01:19 -0300

    Start Echoes Absolute from main menu

--- core/src/main/java/io/github/some_example_name/Main.java
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -1,12 +1,12 @@
 package io.github.some_example_name;
 
 import com.badlogic.gdx.Game;
-import io.github.some_example_name.screens.LuaScreen;
+import io.github.some_example_name.screens.MenuScreen;
 
 public class Main extends Game {
 
     @Override
     public void create() {
-        setScreen(new LuaScreen(this));
+        setScreen(new MenuScreen(this));
     }
 }
