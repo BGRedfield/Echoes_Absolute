822898122a42ab5d16f81e7d7a76e393ca097bb8
BG <arthurazulbg@gmail.com>
2026-09-17 15:14:17 -0300

    Add Lua mission states

---
+++ core/src/main/java/io/github/some_example_name/entities/LuaMission.java
@@ -0,0 +1,12 @@
+package io.github.some_example_name.entities;
+
+/**
+ * Progression of the Lua level.
+ */
+public enum LuaMission {
+    COLLECT_ICE,
+    MELT_ICE,
+    DEFEAT_AMERICAN,
+    DEFEAT_TRUMP,
+    GO_TO_MARS
+}
