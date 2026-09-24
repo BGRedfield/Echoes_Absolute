d709b02e3dfca1a7a47c2ccc9e2468cf98df6c1c
BG <arthurazulbg@gmail.com>
2026-09-24 16:17:30 -0300

    Usa fonte pixelada no TitaIntroScreen.java

--- core/src/main/java/io/github/some_example_name/screens/TitaIntroScreen.java
+++ core/src/main/java/io/github/some_example_name/screens/TitaIntroScreen.java
@@ -36,7 +36,7 @@ public class TitaIntroScreen extends ScreenAdapter {
         this.saveData = saveData;
         batch = new SpriteBatch();
         shapeRenderer = new ShapeRenderer();
-        font = new BitmapFont();
+        font = PixelFontFactory.create();
         viewport = new ScreenViewport();
     }
 
