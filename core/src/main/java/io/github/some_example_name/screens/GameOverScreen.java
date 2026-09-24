73f464e2af1bd7eee5dbd3b2647ba2291bd45560
BG <arthurazulbg@gmail.com>
2026-09-24 16:25:26 -0300

    Volta fonte normal em GameOverScreen.java

--- core/src/main/java/io/github/some_example_name/screens/GameOverScreen.java
+++ core/src/main/java/io/github/some_example_name/screens/GameOverScreen.java
@@ -41,7 +41,7 @@ public class GameOverScreen extends ScreenAdapter {
         this.retryPhase = retryPhase;
         batch = new SpriteBatch();
         shapeRenderer = new ShapeRenderer();
-        font = PixelFontFactory.create();
+        font = new BitmapFont();
         viewport = new ScreenViewport();
     }
 
