fd5909edf3432f6c7416513d2bd79f717b6f0fb2
BG <arthurazulbg@gmail.com>
2026-09-24 16:17:11 -0300

    Usa fonte pixelada no MarsLevelStatusScreen.java

--- core/src/main/java/io/github/some_example_name/screens/MarsLevelStatusScreen.java
+++ core/src/main/java/io/github/some_example_name/screens/MarsLevelStatusScreen.java
@@ -31,7 +31,7 @@ public class MarsLevelStatusScreen extends ScreenAdapter {
         this.hunger = hunger;
         this.oxygen = oxygen;
         batch = new SpriteBatch();
-        font = new BitmapFont();
+        font = PixelFontFactory.create();
         viewport = new ScreenViewport();
     }
 
