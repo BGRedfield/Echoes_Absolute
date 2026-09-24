0093800cea0a57e651bfc03261345234c1a319a1
BG <arthurazulbg@gmail.com>
2026-09-24 16:16:38 -0300

    Usa fonte pixelada no CalistoInfoScreen.java

--- core/src/main/java/io/github/some_example_name/screens/CalistoInfoScreen.java
+++ core/src/main/java/io/github/some_example_name/screens/CalistoInfoScreen.java
@@ -36,7 +36,7 @@ public class CalistoInfoScreen extends ScreenAdapter {
         this.game = game;
         this.saveData = saveData;
         batch = new SpriteBatch();
-        font = new BitmapFont();
+        font = PixelFontFactory.create();
         viewport = new ScreenViewport();
     }
 
