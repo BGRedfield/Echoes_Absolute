09b25784055cb2972fa17f95a0a2199d94168984
BG <arthurazulbg@gmail.com>
2026-09-24 16:25:10 -0300

    Volta fonte normal em AharinInfoScreen.java

--- core/src/main/java/io/github/some_example_name/screens/AharinInfoScreen.java
+++ core/src/main/java/io/github/some_example_name/screens/AharinInfoScreen.java
@@ -36,7 +36,7 @@ public class AharinInfoScreen extends ScreenAdapter {
         this.game = game;
         this.saveData = saveData;
         batch = new SpriteBatch();
-        font = PixelFontFactory.create();
+        font = new BitmapFont();
         viewport = new ScreenViewport();
     }
 
