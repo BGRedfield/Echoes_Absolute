7a052104e9e778bffff1cfe54e52f53c05a4983c
BG <arthurazulbg@gmail.com>
2026-09-24 16:16:47 -0300

    Usa fonte pixelada no FastTravelScreen.java

--- core/src/main/java/io/github/some_example_name/screens/FastTravelScreen.java
+++ core/src/main/java/io/github/some_example_name/screens/FastTravelScreen.java
@@ -52,7 +52,7 @@ public class FastTravelScreen extends ScreenAdapter {
         this.game = game;
         this.saveData = saveData;
         batch = new SpriteBatch();
-        font = new BitmapFont();
+        font = PixelFontFactory.create();
         viewport = new ScreenViewport();
 
         selectedIndex = currentIndex();
