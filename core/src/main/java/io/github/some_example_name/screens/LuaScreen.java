86174e33c6eb979703e6e65e0adec37f5022bb3e
BG <arthurazulbg@gmail.com>
2026-09-24 16:17:04 -0300

    Usa fonte pixelada no LuaScreen.java

--- core/src/main/java/io/github/some_example_name/screens/LuaScreen.java
+++ core/src/main/java/io/github/some_example_name/screens/LuaScreen.java
@@ -146,7 +146,7 @@ public class LuaScreen extends ScreenAdapter {
         hudViewport = new ScreenViewport();
         batch = new SpriteBatch();
         shapeRenderer = new ShapeRenderer();
-        hudFont = new BitmapFont();
+        hudFont = PixelFontFactory.create();
         assets = new AssetManager();
         assets.load();
         player = new Player(PLAYER_SPAWN_X, PLAYER_SPAWN_Y);
