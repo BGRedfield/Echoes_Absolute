83adcf1fb62e57feca116ded1e7c8b49f758f194
BG <arthurazulbg@gmail.com>
2026-09-24 16:17:00 -0300

    Usa fonte pixelada no LuaMarteScreen.java

--- core/src/main/java/io/github/some_example_name/screens/LuaMarteScreen.java
+++ core/src/main/java/io/github/some_example_name/screens/LuaMarteScreen.java
@@ -143,7 +143,7 @@ public class LuaMarteScreen extends ScreenAdapter {
         hudViewport = new ScreenViewport();
         batch = new SpriteBatch();
         shapeRenderer = new ShapeRenderer();
-        hudFont = new BitmapFont();
+        hudFont = PixelFontFactory.create();
         assets = new AssetManager();
         assets.load();
         player = new Player(PLAYER_SPAWN_X, PLAYER_SPAWN_Y);
